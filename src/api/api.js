b2153e9a584496663df609c6b8c10722f89fe601
byer <byer.wu@gmail.com>
2018-07-13 22:07:53 +0800

    update mock data

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,30 @@
 /* eslint-disable */
 module.exports = {
-  getUser: '/back/user',
-  showsupervisor: '/back/supervisor/showsupervisor',
+  login: '/back/login', // 登录
+  showsupervisor: '/back/supervisor/showsupervisor', // 系统管理-管理员列表接口
+  showsupervisordetail: '/back/supervisor/showsupervisordetail', // 系统管理-管理员添加和更新的回显信息
+  editsupervisor: '/back/supervisor/editsupervisor', // 系统管理-更新管理员接口
+  addsupervisor: '/back/supervisor/addsupervisor', // 系统管理-添加管理员接口
+  delsupervisor: '/back/supervisor/delsupervisor', // 系统管理-删除管理员接口
+  showsupervisoreventlog: '/back/supervisor/showsupervisoreventlog', // 系统日志管理-操作日志列表接口
+  showrightrole: '/back/role/showrightrole', // 系统管理-角色列表接口
+  showrightroledetail: '/back/role/showrightroledetail', // 系统管理-角色更新的回显信息
+  editrightrole: '/back/role/editrightrole', // 系统管理-更新角色接口
+  addrightrole: '/back/role/addrightrole', // 系统管理-添加角色接口
+  showright: '/back/role/showright', // 系统管理-查询该角色的权限信息接口
+  editright: '/back/role/editright', // 系统管理-编辑该角色的权限信息接口
+  showrunruledetail: '/back/setting/showrunruledetail', // 运营设置-查询运营规则接口
+  editrunrule: '/back/setting/editrunrule', // 运营设置-编辑运营规则接口
+  showrunruledetail: '/back/setting/showrunruledetail', // 运营设置-查询分销规则接口
+  editrunrule: '/back/setting/editrunrule', // 运营设置-编辑分销规则接口
+  showwithdrawuser: '/back/account/showwithdrawuser', // 提现管理-提现列表接口
+  showwithdrawuserdetail: '/back/account/showwithdrawuserdetail', // 提现管理-提现详情接口
+  auditwithdrawuser: '/back/account/auditwithdrawuser', // 提现管理-提现审核接口
+  liststoreandareas: '/back/employee/liststoreandareas', // 架构管理-架构列表接口
+  addstorearea: '/back/employee/addstorearea', // 架构管理-添加大区接口
+  showstoreareadetail: '/back/employee/showstoreareadetail', // 架构管理-查询要编辑的大区接口
+  editstorearea: '/back/employee/editstorearea', // 架构管理-编辑大区接口
+  addstore: '/back/employee/addstore', // 架构管理-添加门店接口
+  showstoredetail: '/back/employee/showstoredetail', // 架构管理-查询要编辑的门店接口
+  editstore: '/back/employee/editstore', // 架构管理-编辑门店接口
 }
